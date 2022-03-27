b8816eb2c850eaa740440515e527047b62a2dffc
Abdullah <abdullahyahaya46@gmail.com>
2022-03-27 15:51:08 +0100

    Added Backend Environment

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+const bodyParser  = require('body-parser');
+const $ = require('jquery');
+
 
 // Load config variables
 dotenv.config({ path: './config/config.env' });
@@ -15,6 +18,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // calling the function
 connectDB();
+
+// fecth data from the request
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// set the path of the query file to be used from the node_modules jquery
+app.use('/jquery', express.static(path.join(__dirname+'/node_modules/jquery/dist')))
+
+// setting routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
 
