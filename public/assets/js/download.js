5b3657435975cd861376dcd17de52952b6b23c31
Abdullah <abdullahyahaya46@gmail.com>
2022-05-16 00:46:22 +0100

    Made some corrections and add some few things

--- public/assets/js/download.js
+++ public/assets/js/download.js
@@ -1,6 +1,6 @@
 function download() {
     axios({
-          url: 'http://localhost:3000',
+          url: 'http://localhost:3000/assets/files/Resume.pdf',
           method: 'GET',
           responseType: 'blob'
     })
@@ -9,7 +9,7 @@ function download() {
                       .createObjectURL(new Blob([response.data]));
                 const link = document.createElement('a');
                 link.href = url;
-                link.setAttribute('download', 'abdullah.pdf');
+                link.setAttribute('download',   `Abdullah's Resume.pdf`);
                 document.body.appendChild(link);
                 link.click();
                 document.body.removeChild(link);
